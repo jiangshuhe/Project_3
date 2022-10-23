8ad2b3cd13c23f13ee5a8f04b59ad3d6e3bb7551
jiangshuhe <shuhej@andrew.cmu.edu>
2022-10-23 02:41:52 -0700

    try

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ export default {
   build: {
     target: ["chrome100", "safari15", "firefox100"],
   },
-  // Change this to your repo name
+  // Change this to your repo names
   base: "/D3-App-Template/",
 };
